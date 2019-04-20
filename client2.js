04d03b2814a837ee956a1c4a60a1243575e5db89
ChristianSwift <topman@dingnaiwen.xyz>
2019-04-20 14:33:46 +0800

    init

---
+++ client2.js
@@ -0,0 +1,17 @@
+const WebSocket = require('ws');
+const FogID = '0123';
+const ws = new WebSocket('ws://127.0.0.1:9000/'+FogID);
+ws.onopen = function(){  
+    console.log("WS Connection Start.");
+    ws.send("hello");
+};
+ws.onmessage = function(evt){
+    console.log(evt.data)
+    ws.send("Received!")
+};
+ws.onclose = function(evt){
+    console.log("WS Connection Closed!");
+};
+ws.onerror = function(evt){
+    console.log("WS Connection Error!");
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+const readline = require('readline');
+const WebSocket = require('ws');
+const cmd = readline.createInterface(process.stdin, process.stdout);
+clients = new Array();
+const wss = new WebSocket.Server({ port: 9000 });
+
+cmd.setPrompt('Cloud Console> ');
+cmd.prompt();
+
+wss.on('connection', function connection(res, req) {
+    clients[req.url.split("/")[1]]=res;
+    res.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    cmd.on('line', function(line) {
+        line = line.trim();
+        switch(line.split(" ")[0]){
+            case "help":
+                console.log("Type 'show' to see all fog.");
+                break;
+            case "show":
+                console.log(clients);
+                break;
+            case "open":
+                if (line.split(" ")[1].length!=8){
+                    console.log("Wrong lock ID, Please check.")
+                }else{
+                    console.log("Lock No."+line.split(" ")[1]+" will open!");
+                }
+                break;
+            case "com":
+                try {
+                    clients[line.split(" ")[1]].send(line.split(" ")[2]);
+                } catch (e) {
+                    console.warn("Fog do not exist or invalid!");
+                }
+                break;
+            case "bdc":
+                res.send("echo");
+                break;
+            default:
+                console.warn("Unacceptable command!");
+                break;
+        }
+        cmd.prompt();
+    });
+
+    cmd.on('close', function() {
+        console.log('Terminated!');
+        process.exit(0);
+    });
+});
